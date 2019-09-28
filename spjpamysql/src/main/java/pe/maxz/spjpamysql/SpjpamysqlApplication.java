948bc9e5e86928990ad3b5078911ac7a0c981c59
Max Zavaleta Villar <Max.Zavaleta@oechsle.pe>
2019-09-28 11:52:37 -0500

    carga inicial

---
+++ spjpamysql/src/main/java/pe/maxz/spjpamysql/SpjpamysqlApplication.java
@@ -0,0 +1,13 @@
+package pe.maxz.spjpamysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpjpamysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpjpamysqlApplication.class, args);
+	}
+
+}
